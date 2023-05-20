aeda38186b8b0a2138e35f628363b1bc82614dd1
guilherme-marcello <guilemosm@gmail.com>
2023-05-20 11:10:28 +0100

    Added error handling for apsignal

--- src/apsignal.c
+++ src/apsignal.c
@@ -5,14 +5,25 @@
 #include <sys/types.h>
 
 #include "apsignal.h"
+#include "main-private.h"
 
 void set_timer(int inter, void (*handler)(int)) {
-    signal(SIGALRM, handler);
+    verify_condition(
+        signal(SIGALRM, handler) == SIG_ERR,
+        INIT_ALARM,
+        ERROR_REGISTER_SIGNAL_HANDLER,
+        EXIT_REGISTER_SIGNAL_HANDLER_ERROR
+    );
     alarm(inter);
 }
 
 void set_intr_handler(void (*handler)(int)) {
-    signal(SIGINT, handler);
+    verify_condition(
+        signal(SIGINT, handler) == SIG_ERR,
+        INIT_INTR,
+        ERROR_REGISTER_SIGNAL_HANDLER,
+        EXIT_REGISTER_SIGNAL_HANDLER_ERROR
+    );
 }
 
 void interrupt_process(int process_id) {
